6b13e646cb7f951460c1ac7b7d9ff5eecf0e8e4f
Samuel Debono <samuel.debono@envoyat.com>
2025-04-01 13:10:35 +1100

    Revert "fix: add missing dist folder"
    This reverts commit 1264dac05c5659ee9ff3cedc1106f2c695ffb562.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -15,7 +15,6 @@ const CardNumbers = [
   "371449635398431",
   5105105105105100,
   2223003122003222,
-  181213123456789
 ];
 const CardNames = ["", "John Doe"];
 const CardFocusStates: Focused[] = ["", "number", "name", "cvc", "expiry"];
